7b337c20383d71770bf82f58c19569e773f33f2b
Arif ORAK <ariforak91@gmail.com>
2023-02-12 00:54:30 +0300

    Response Durumuna göre aksiyon alacak CustomBaseController classı oluşturuldu,

---
+++ NLayer.WebAPI/Controllers/CustomBaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs.ResponseDto;
+
+namespace NLayer.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response) where T:class
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode,
+                };
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
