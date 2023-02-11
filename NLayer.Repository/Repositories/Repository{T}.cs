41246824a0192c9f031c3771de4c5e76987848c6
Arif ORAK <ariforak91@gmail.com>
2023-02-11 21:37:35 +0300

    GenericRepository classının Temel Operasyonları oluşturuldu. DbContext Protected olarak erişimi düzeltildi, Custom Repository'lerde ihtiyaç olacaktır.

---
+++ NLayer.Repository/Repositories/Repository{T}.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Repository.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsnyc(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+         
+        }
+        //Veriler Memory'e alınmayacak, Business Logic olarak filter uygulanabilir data AsNoTracking().
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        //state.Deleted
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.AsNoTracking().Where(expression); 
+        }
+    }
+}
