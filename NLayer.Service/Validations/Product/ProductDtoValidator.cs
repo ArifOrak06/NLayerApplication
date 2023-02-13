8109c7dbd55dbb868c85d5c465e3a87e3631b758
Arif ORAK <ariforak91@gmail.com>
2023-02-13 22:04:50 +0300

    FluentValidation Kütüphanesi Uygulamaya dahil edildi.

---
+++ NLayer.Service/Validations/Product/ProductDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs.ProductDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Validations.Product
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} null olamaz").NotEmpty().WithMessage("{PropertyName} alanı zorunlu bir alandır.");
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı 0 rakamından büyük bir değer olmalı");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı 0 rakamından büyük bir değer olmalı");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} alanı 0 rakamından büyük bir değer olmalı");
+        }
+    }
+}
